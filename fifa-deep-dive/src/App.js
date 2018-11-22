c8d2eb2723f252c5f26bf3f1b11409556e6665e3
Adrian Viquez <aviquez96@gmail.com>
2018-11-22 00:06:49 -0800

    initial commit

---
+++ fifa-deep-dive/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Board from './Components/Board.jsx'
+import Player from './Components/Player.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Player />
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default App;
